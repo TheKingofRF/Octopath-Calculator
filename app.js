c4bc845f5b7c24ecab5f390e2607d651b2b00dcf
Mason Unger <masonunger97@gmail.com>
2022-03-29 21:07:27 -0500

    JSON working layout reworked. CSS still needed.

--- app.js
+++ app.js
@@ -4,7 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const victoryBtn = document.querySelector('#done')
     const opsBtn = document.querySelector('#opps')
     const setBtn = document.querySelector('#set')
-    const setterbox = document.querySelector('#setter')
+    //const setterbox = document.querySelector('#setter')
+    const batXp = document.querySelector('#expPerBat')
+    const CurLvl = document.querySelector('#CurLvl')
+    const goalLvl = document.querySelector('#goalLvl')
+    const xpDisplay = document.querySelector('#XpleftStat')
+    const CurLvlStat = document.querySelector('#CurLvlStat')
+    const lvlDisplay = document.querySelector('#LvlsLeftstat')
+    async function popLvls(){
+        const requestURL = 'https://raw.githubusercontent.com/TheKingofRF/Octopath-Calculator/main/test.jsonc'
+        const request = new Request(requestURL);
+        const response = await fetch(request);
+        const LvlList = await response.json();
+        test(LvlList);
+    }
+    popLvls()
+    function test (obj) {
+        console.log(obj.data[0])
+    }
     function newScore () {
         battleDisplay.innerHTML = battles
         if (battles <= 0){
